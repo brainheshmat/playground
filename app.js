60004a5052912facbdf9e8e8b73d69310febb996
sushmit sarmah <sushmitsarmah@gmail.com>
2015-03-10 19:59:50 +0530

    added popups

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var busboy = require('connect-busboy');
+var fs = require('fs');
 
 var app = express();
 
@@ -15,6 +17,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(busboy());
 app.use(cookieParser());
 app.use(express.static(__dirname));
 
@@ -22,7 +25,18 @@ app.get('/', function(req, res){
     res.render("index");
 });
 
-app.get('/getdata', function(req, res){});
+app.post('/getdata', function(req, res){
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function (fieldname, file, filename) {
+        console.log("Uploading: " + filename); 
+        fstream = fs.createWriteStream(__dirname + '/files/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function () {
+            res.redirect('back');
+        });
+    });
+});
 
 app.set('port', process.env.PORT || 3000);
 
